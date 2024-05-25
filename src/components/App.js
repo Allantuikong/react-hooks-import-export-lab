47885b1912a0333430679717d0eea2f48d34c1e8
Allan Kipkorir <allantuix@gmail.com>
2024-05-25 15:12:35 +0300

    Completed react import/export lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 
 function App() {
+  console.log("App component");
   return (
     <div>
       <NavBar />
